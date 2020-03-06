ec90181db4f164b8c3d5eb9c4a9672cd8c8b15d5
Thibault-21 <thib.dec.pro@gmail.com>
2020-03-06 11:11:16 +0100

    second commit

---
+++ components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main">
+    <nav class="spacing">
+      <nuxt-link class="routes" to="/">Home</nuxt-link>
+      <nuxt-link class="routes" to="/about">About</nuxt-link>
+      <nuxt-link class="routes" to="/blog">Blog</nuxt-link>
+      <nuxt-link class="routes" to="/contact">Contact</nuxt-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="main">
+    <Navbar></Navbar>
+    <main> 
+      <nuxt />
+    </main>
+    <footer>
+&copy 2020 - test i18n with Nuxt.js
+    </footer>
+   
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/NavBar'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+}
+.main {
+  font-family: 'Roboto', sans-serif;
+}
+nav {
+  background-color: rgb(27, 27, 50);
+  padding: 20px;
+  color: white;
+}
+main {
+  height: 100%;
+  padding: 10px;
+  border-radius: 5px;
+  margin: 5px;
+  border: 1px solid rgb(27, 27, 50);
+}
+
+ footer {
+  background-color: rgb(238, 238, 240);
+  color: rgb(59, 59, 79);
+  padding: 25px;
+  margin: 5px;
+  border-radius: 5px;
+}
+
+.spacing{
+  /* color: white; */
+  margin: 5px;
+  padding: 35px;
+  border-radius: 5px;
+}
+.routes {
+  color: white;
+  margin-right: 35px;
+  text-decoration: none;
+  font-size: 20px;
+}
+</style>
